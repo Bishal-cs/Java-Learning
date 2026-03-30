5a9426aee2002e9d5ecfc34363ccf696e86a7ceb
Bishal-cs <bishaldas1152006@gmail.com>
2026-03-30 11:54:27 +0530

    1st prj on java

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Two Numbers: ");
+		
+		int value1 = sc.nextInt();
+		int value2 = sc.nextInt();
+		
+		System.out.println("The Add = "+(value1 + value2));
+		System.out.println("The Sub = "+(value1 - value2));
+		System.out.println("The Mul = "+(value1 * value2));
+		System.out.println("The Div = "+((float)value1 / value2));
+
+	}
+
+}
